432900d9d17163218be4566c778cd52c328bb4d5
Giulio Paci <giuliopaci@gmail.com>
2017-08-19 14:20:42 +0200

    Detect Mac OSX using compiler pre-defined macros instead of custom ones.
    A list of predefined macros to match several compiler
    features is available at:
    http://nadeausoftware.com/articles/2012/01/c_c_tip_how_use_compiler_predefined_macros_detect_operating_system

--- src/lib/util.cc
+++ src/lib/util.cc
@@ -158,7 +158,7 @@ DEFINE_bool   (help, false, "show usage information");
 void PhonetisaurusSetFlags (const char* usage, int* argc, char*** argv,
 			    bool remove_flags) {
   //Workaround for Apple's. It just skips all the options processing. 
-#ifdef DARWIN
+#if defined(__APPLE__) && defined(__MACH__)
   SetFlags (usage, argc, argv, remove_flags);
 #else
   int index = 1;
